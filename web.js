84227d7f4cff85c81d4223e1d4a7feb139e17ca8
Ubuntu <ubuntu@ip-172-31-35-184.us-west-2.compute.internal>
2013-07-11 13:03:18 +0000

    changed web.js to use indext.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var content = fs.readFileSync('index.html', 'utf-8');
+  var content = fs.readFileSync('index.html').toString();
   response.send(content);
 });
 
