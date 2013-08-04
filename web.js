2a137714e1abffbcd472a401418c37cde6a67290
xiaoguo <guox652@gmail.com>
2013-08-04 01:37:43 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
-
+var fs = require('fs');
+var htmlfile = "index.html";
+ 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fs = require('fs');
-  var content = fs.readFileSync('index.html').toString();
-  response.send(content);
+  var html = fs.readFileSync('index.html').toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
